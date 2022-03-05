555517d90c683de5efd3f2afa1ade91fe486ffc6
Alexsander Mikhaylov <mihaleksval@gmail.com>
2022-03-05 14:56:15 +0500

    Navigation programmatically eleven

--- hello-world/pages/index.js
+++ hello-world/pages/index.js
@@ -1,7 +1,16 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+
 export default function Home() {
   // Link component to path with slash - link included in the component
   // With the anchor tag with the text and the href for the route to the component
+  const router = useRouter();
+
+  const handleClick = () => {
+    console.log('Placing you order');
+    router.replace('/product');
+  };
+
   return (
     <div style={{ textAlign: 'center' }}>
       <h1>Home page</h1>
@@ -24,12 +33,18 @@ export default function Home() {
               <a>Link to Blog first</a>
             </Link>
           </li>
+
           <li>
             <Link href='/product'>
               <a>Link to product</a>
             </Link>
           </li>
-        </ul>
+        </ul>{' '}
+        And the same thing as <i>Link to Product</i> like example below <br />
+        {/* Navigate Programmatically */}
+        <button className='' onClick={handleClick}>
+          Place Order
+        </button>
       </nav>
     </div>
   );
