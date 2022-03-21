86f5d9f82a3643caf89d5557965df851f7247b31
Alexsander Mikhaylov <mihaleksval@gmail.com>
2022-03-21 15:10:58 +0500

    50 global styles adding bootstrap and some dependencies

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function About() {
-  return <h1>About page</h1>;
+  const [color, setColor] = useState(false);
+  return (
+    <div>
+      <h2>About page</h2>
+      {color ? (
+        <div>
+          <button className='btn btn-primary' onClick={() => setColor(!color)}>
+            Primary or success
+          </button>
+        </div>
+      ) : (
+        <div>
+          <button className='btn btn-success' onClick={() => setColor(!color)}>
+            Primary or success
+          </button>
+        </div>
+      )}
+    </div>
+  );
 }
