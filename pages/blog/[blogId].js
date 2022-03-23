3dd7080a82495f542b091e42a1d64204ec22c1a9
Alexsander Mikhaylov <mihaleksval@gmail.com>
2022-03-23 23:37:40 +0500

    64 environment variables

--- pages/blog/[blogId].js
+++ pages/blog/[blogId].js
@@ -13,11 +13,23 @@ export default function Blog({ title, description }) {
         <meta name='description' content={description} />
       </Head>
       <h1 className='content'>Article</h1>
+      <h2 style={{ textAlign: 'center' }}>{description}</h2>
+      <p style={{ textAlign: 'center' }}>
+        {/* {process.env.DB_USER} - {process.env.DB_PASSWORD} */}
+        <br />
+        {process.env.NEXT_PUBLIC_ANALYTICS_ID}
+      </p>
     </div>
   );
 }
 
 export async function getServerSideProps() {
+  // 64 ENV variable
+  const user = process.env.DB_USER;
+  const password = process.env.DB_PASSWORD;
+
+  console.log(`Connected user with name = ${user} and password = ${password}`);
+
   return {
     props: {
       title: 'Article Title',
